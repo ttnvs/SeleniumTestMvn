19efd3626356d01639c1064ec45198f5ebe9b4ad
tatiana <tatiana.vvs@gmail.com>
2021-08-25 00:49:48 +0100

    Initial commit

---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.*;
+
+
+public class TestSuite {
+
+    @Test
+    public void test1() {
+        // System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver_v92.exe");
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver_mac_v92");
+		WebDriver driver = new ChromeDriver();
+
+		//Please write your code here
+        System.out.println("Hello world");
+
+        driver.close();
+    }
+
+}
