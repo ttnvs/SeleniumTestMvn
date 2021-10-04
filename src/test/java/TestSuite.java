ddc8eaab2f381f02717697524ae34ef23d148019
tatiana <tatiana.vvs@gmail.com>
2021-10-04 23:15:56 +0100

    Add chromdriver v94 for mac

--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -9,7 +9,7 @@ public class TestSuite {
     @Test
     public void test1() {
          System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver_v94.exe");
-//        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver_mac_v92");
+//        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver_mac_v94");
         WebDriver driver = new ChromeDriver();
 
         //Please write your code here
